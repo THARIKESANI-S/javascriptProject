a10b9630083bc5778bd6e5651d8796dd963044ed
THARIKESANI-S <tharikesani.s2022csbs@sece.ac.in>
2024-07-23 18:52:14 +0530

    change2

---
+++ colorPicker/colorPicker.js
@@ -0,0 +1,19 @@
+function changeBackgroundWhite() {
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#e0e0e0";
+}
+
+function changeBackgroundGreen() {
+    document.getElementById("selectedColorHexCode").textContent = "#6fcf97";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#6fcf97";
+}
+
+function changeBackgroundBlue() {
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#56ccf2";
+}
+
+function changeBackgroundPurple() {
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("colorPickerContainer").style.backgroundColor = "#bb6bd9";
+}
